5b11538d05477319dc6322418af032d613891bda
王含泽 <whzcorcd@gmail.com>
2018-07-25 12:11:59 +0800

    更新热射电模块

--- Pi_PES.py
+++ Pi_PES.py
@@ -2,15 +2,17 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(5, GPIO.IN)
+Pin = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Pin, GPIO.IN)
 
 try:
-  while True:
-    if GPIO.input(5) == 1:
-      print ("Some people here!")
-    else:
-      print ("Nobody!")
-    time.sleep(.1)
+    while True:
+        if GPIO.input(Pin) == 1:
+            print("Some people here!")
+        else:
+            print("Nobody!")
+        time.sleep(1)
 except KeyboardInterrupt:
-  GPIO.cleanup()
+    GPIO.cleanup()

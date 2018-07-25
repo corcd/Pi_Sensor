20073ce71d10943cd4d51c2b736cc61af0212212
王含泽 <whzcorcd@gmail.com>
2018-07-25 15:50:49 +0800

    调整结构

--- Trigger.py
+++ Trigger.py
@@ -4,39 +4,37 @@ import time
 
 trigger = 1  #触发器是否有效，默认有效
 
+# def screen_trigger():
+#     return []
 
-def screen_trigger():
-    return []
+#def face_trigger(trigger):
+try:
+    while True:
+        someone = os.popen('python3 core/Pi_PES.py').read()
+        distance1 = os.popen('python3 core/Pi_PCSR.py').read()
+        time.sleep(1)
+        distance2 = os.popen('python3 core/Pi_PCSR.py').read()
 
+        print(someone, distance1, distance2)
+        # if isinstance(someone, bytes):
+        #     s = str(someone, encoding='utf-8')
+        # if isinstance(distance1, bytes):
+        #     d1 = str(distance1, encoding='utf-8')
+        # if isinstance(distance2, bytes):
+        #     d2 = str(distance2, encoding='utf-8')
 
-def face_trigger(trigger):
-    try:
-        while True:
-            someone = os.popen('python3 core/Pi_PES.py').read()
-            distance1 = os.popen('python3 core/Pi_PCSR.py').read()
-            time.sleep(1)
-            distance2 = os.popen('python3 core/Pi_PCSR.py').read()
+        if someone == 'true' & distance1 < 100 & distance1 > 30 & distance2 < 100 & distance2 > 30 & trigger == 1:
+            chromedriver = '/home/pi/Downloads/chromedriver'
+            os.environ['webdriver.chrome.driver'] = chromedriver
+            driver = webdriver.Chrome(chromedriver)  #模拟打开浏览器
+            driver.get('http://www.baidu.com/')  #打开网址
+            driver.maximize_window()  #窗口最大化
+            trigger = 0
+        else:
+            trigger = 1
+        print(trigger)
+        time.sleep(2)
+except KeyboardInterrupt:
+    pass
 
-            print(someone,distance1,distance2)
-            # if isinstance(someone, bytes):
-            #     s = str(someone, encoding='utf-8')
-            # if isinstance(distance1, bytes):
-            #     d1 = str(distance1, encoding='utf-8')
-            # if isinstance(distance2, bytes):
-            #     d2 = str(distance2, encoding='utf-8')
-
-            if someone == 'true' & distance1 < 100 & distance1 > 30 & distance2 < 100 & distance2 > 30 & trigger == 1:
-                chromedriver = "/home/pi/Downloads/chromedriver"
-                os.environ["webdriver.chrome.driver"] = chromedriver
-                driver = webdriver.Chrome(chromedriver)  #模拟打开浏览器
-                driver.get("https://www.baidu.com/")  #打开网址
-                driver.maximize_window()  #窗口最大化
-                trigger = 0
-            else:
-                trigger = 1
-            print(trigger)
-            time.sleep(2)
-    except KeyboardInterrupt:
-        return []
-
-face_trigger(trigger)
+#face_trigger(trigger)
